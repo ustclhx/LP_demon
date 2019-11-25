18f2816eca868f7b4e4abfe90bcecbfeb53d7910
ustclhx <lhx960924@gmail.com>
2019-11-25 20:34:58 +0800

    newgraph

--- src/graph/graph.go
+++ src/graph/graph.go
@@ -1,19 +1,49 @@
 package graph
 
 import(
-
+	"fmt"
+	//"errors"
 )
 
-type graph struct{
+type Graph struct{
 	nodes []*Node 
 	edges map[Node]map[Node]*Edge
-
 }
 
-func (g graph) AddNode(n *Node){
-
+func (g Graph) IsNodeIn(n Node) bool{
+	_,ok := g.edges[n]
+	return ok
 }
 
-func (g graph) AddEdge()(e *Edge){
-
+func NewGraph(ns []*Node, es []*Edge) (g *Graph,err error){
+	edges := make(map[Node]map[Node]*Edge)
+	for  _, node := range ns{
+		edges[*node]= make(map[Node]*Edge)
+	}
+	for _, e := range es{
+		_,ok_0 := edges[*e.nodes[0]]
+		_,ok_1 := edges[*e.nodes[1]]
+		if !(ok_0 && ok_1){
+			err =  fmt.Errorf("the edge between node %s and node %s has a node not in the graph",e.nodes[0].name,e.nodes[1].name)
+			return 
+		}
+		edges[*e.nodes[0]][*e.nodes[1]]= e
+		edges[*e.nodes[1]][*e.nodes[0]]= e	
+	}
+	g = &Graph{
+		nodes : ns,
+		edges : edges,
+	}
+	return 
 }
+
+func (g *Graph) AddNode(n *Node){
+	g.nodes = append(g.nodes,n)
+}
+
+/*func (g *Graph) AddEdge(e *Edge)(err error){
+	if g,edges == nil{
+		g.edges = make(map[Node]map[Node]*Edge)
+	}
+}*/
+
--- src/graph/test_test.go
+++ src/graph/test_test.go
@@ -3,6 +3,7 @@ package graph
 import(
 	"testing"
 	"fmt" 
+	"strconv"
 )
 
 func TestNode(t *testing.T){
@@ -16,13 +17,36 @@ func TestNode(t *testing.T){
 func TestEdge(t *testing.T){
 	A := NewDefaultNode("x")
 	B := NewNode("y",false)
-	e:=NewDefaultEdge(A,B)
+	e := NewDefaultEdge(A,B)
 	fmt.Println(e)
-	e=NewEdge(A,Arrow,B,Tail)
+	e = NewEdge(A,Arrow,B,Tail)
 	fmt.Println(e)
 	B.Setob(true)
 	fmt.Println(A.Isob(),B.Isob(),e.Isob())
 	fmt.Println(e)
 	e=NewEdge(A,Undefined,B,Arrow)
 	fmt.Println(e)
+}
+
+func TestNewGraph(t *testing.T){
+	testnum := 3
+	nodes := make([]*Node,0) 
+	edges := make([]*Edge,0)
+	for i :=0;i<testnum;i++{
+		node := NewDefaultNode(strconv.Itoa(i))
+		nodes = append(nodes,node)
+	}
+	for i :=0;i+1<testnum;i++{
+		edge := NewEdge(nodes[i],Tail,nodes[i+1],Arrow)
+		edges = append(edges,edge)
+	}
+	errnode :=NewDefaultNode(strconv.Itoa(testnum+1))
+	edges = append(edges,NewEdge(nodes[0],Arrow,errnode,Tail))
+	_,err := NewGraph(nodes,edges)
+    if err ==nil{
+		fmt.Println("OK")
+	}else{
+		fmt.Println(err.Error())
+	}
+
 }
